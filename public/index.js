7f2006010d53c8e74aa2fb6c4b470f2cedfc13c6
F-Scienza <cufa1905@gmail.com>
2021-12-06 10:47:55 -0300

    primer commit

---
+++ public/index.js
@@ -0,0 +1,10 @@
+//  ya podemos usar los sockets
+const socket = io()
+
+//  recibimos data del dservidor 
+//  y se los mostramos al cliente
+socket.on( 'mi mensaje', data => {
+    console.log(data)
+    //  tambien podemos enviar mensaje al servidor  
+    socket.emit('notificacion', 'mensaje enviado con exito')
+} )   
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const { Server: IOServer } = require('socket.io')
+const { Server: HttpServer } = require('http')
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+//  indicamos el uso de archivos estaticos de public
+app.use(express.static('./public'))
+//  Cargamos el html en la raiz 
+app.get('/', (req, res)=>{
+    res.sendFile('index.html', {root: __dirname})
+})
+
+//  http server escuchando puerto 3000
+httpServer.listen(3000, ()=> console.log('server ON'))
+
+// Esto se ejecuta solo la primera vez 
+// que se abre una nueva conexión
+
+io.on('connection', (socket)=>{
+    console.log('usuario conectado')
+    //  .emit recibe dos parametros
+    //  nombre del evento y informacion a transmitir
+    socket.emit('mi mensaje', 'primer mensaje desde el servidor')
+})
+
+
+//  envio de datos a todos los us conectados
+/*
+io.on('conection', (socket)=>{
+    console.log('nuevo usuario conectado')
+    //  envio los mensajes al cliente que se conectó
+    socket.emit('mensaje', mensajes)
+
+    //  escucho los mensajes enviados por el cliente 
+    //  y se los mando a todos
+    socket.on('mensaje', data => {
+        mensajes.push({ socketid: socket.id, mensaje: data })
+        // enviamos un mensaje global a todos los conectados
+        io.sockets.emit('mensajes', mensajes )
+    })
+})*/
